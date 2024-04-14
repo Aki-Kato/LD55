f1906278e21d17b32184aa08f432356958cb8de9
Steventus <56802489+steventus@users.noreply.github.com>
2024-04-14 22:30:53 +0800

    Update FestivalEventFactory.cs

--- Assets/Scripts/WorldEvents/Factories/FestivalEventFactory.cs
+++ Assets/Scripts/WorldEvents/Factories/FestivalEventFactory.cs
@@ -6,21 +6,21 @@ namespace WorldEvent
 {
     public class FestivalEventFactory : BaseEventFactory
     {
-        [SerializeField] private GameObject festivalEventPrefab;
+        [SerializeField] private FestivalEvent festivalEventPrefab;
         public override BaseEvent CreateEvent(Vector3 position,float _rotation, Vector3 collidersSize)
         {
             //Instantiate Prefab in a Vector3 position, rotated around Y-axis by _rotation angles.
-            GameObject instance = Instantiate(festivalEventPrefab.gameObject, position, Quaternion.Euler(0,_rotation,0));
+            FestivalEvent instance = Instantiate(festivalEventPrefab, position, Quaternion.Euler(0,_rotation,0));
 
             //Set Colliders Size based on length and width only. Height is ignored.
             BoxCollider collider = instance.GetComponent<BoxCollider>();
             collider.size = collidersSize;
 
-            FestivalEvent festivalEvent = instance.GetComponent<FestivalEvent>();
-            festivalEvent.eventDuration = eventDurationToSet;
-            festivalEvent.Initialise();
+            
+            instance.eventDuration = eventDurationToSet;
+            instance.Initialise();
 
-            return festivalEvent;
+            return instance;
         }
     }
 }
